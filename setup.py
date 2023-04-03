f4a0941293603468ba946c4d13788ef13a3d0b07
JavierSC <74877098+JavSensei@users.noreply.github.com>
2023-04-03 13:17:02 -0500

    Actualizar setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pcloud-dl",
-    version="0.1.4",
+    version="0.1.5",
     author="JavierSC",
     description="pcloud downloader",
     long_description=long_description,
