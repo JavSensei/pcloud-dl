bb75372766237de0f7a8504574b32dea61b13db5
JavierSC <74877098+JavSensei@users.noreply.github.com>
2023-12-20 03:45:35 -0500

    Support european region
    Add support for eapi.pcloud.com endpoint

--- pcloud_dl/cli.py
+++ pcloud_dl/cli.py
@@ -2,11 +2,8 @@ import argparse, re
 from .downloader import download_file
 
 try:
-
     from pcloud import PyCloud
-
-except:
-
+except ImportError:
     import os
 
     print("installing external requirements from github...")
@@ -17,23 +14,25 @@ except:
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("link_or_code", help="Link o código del archivo de pCloud")
-    # parser.add_argument("--username", help="Nombre de usuario de pCloud")
-    # parser.add_argument("--password", help="Contraseña de pCloud")
+    parser.add_argument("--endpoint", help="Tipo de endpoint (default: api, eapi para el endpoint alternativo)", default="api")
     args = parser.parse_args()
 
-    link_regex = r"^https?://u\.pcloud\.link/publink/show\?code=(?P<code>\w+)$"
+    link_regex = r"^https?://(u|e)\.pcloud\.link/publink/show\?code=(?P<code>\w+)$"
     code_regex = r"^(?P<code>\w+)$"
     link_match = re.match(link_regex, args.link_or_code)
     code_match = re.match(code_regex, args.link_or_code)
+
     if link_match:
         code = link_match.group("code")
+        endpoint = "api" if link_match.group(1) == "u" else "eapi"
     elif code_match:
         code = code_match.group("code")
+        endpoint = args.endpoint
     else:
         print("Error: el argumento debe ser un enlace o un código de pCloud")
         return
 
-    pc = PyCloud("", "")
+    pc = PyCloud("", "", endpoint=endpoint)
 
     response = pc.getpublinkdownload(code=code)
     if response["result"] == 0:
@@ -47,4 +46,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
